64bbf38756040920e91de9feb0f3a59e41529364
sivakartheek <mukkamalasivakartheek@gmail.com>
2025-02-09 09:53:09 +0530

    commit

--- ChatAssistant/ChatAssistant/settings.py
+++ ChatAssistant/ChatAssistant/settings.py
@@ -115,9 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
